618aabf703fd521094ca453b98bbd179d2f210df
unknown <altamashbagwan1@gmail.com>
2021-10-18 22:43:35 +0530

    Added: Functioning of Delete First value using pop method

--- javapractice/MyLinkedListTest.java
+++ javapractice/MyLinkedListTest.java
@@ -47,12 +47,12 @@ public class MyLinkedListTest {
 		myLinkedList.append(myThirdNode);
 		myLinkedList.insert(myFirstNode, mySecondNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) &&
+		boolean result = myLinkedList.head.equals(myFirstNode) && 
 						 myLinkedList.head.getNext().equals(mySecondNode) &&
 						 myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
-	
+
 	@Test
 	public void givenFirstElementWhenDeletedShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
@@ -67,7 +67,7 @@ public class MyLinkedListTest {
 		boolean result = myLinkedList.head.equals(mySecondNode) &&
 						 myLinkedList.head.getNext().equals(myThirdNode) &&
 						 myLinkedList.tail.equals(myThirdNode);
-	Assert.assertTrue(result);
+		Assert.assertTrue(result);
 	}
-	
+
 }
