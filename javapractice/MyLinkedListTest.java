55f1f801bf498d0e8904bb5858ca71f4da6378cd
unknown <altamashbagwan1@gmail.com>
2021-10-18 15:35:43 +0530

    Added: Functioning of Linked List value by Append order

--- javapractice/MyLinkedListTest.java
+++ javapractice/MyLinkedListTest.java
@@ -15,8 +15,9 @@ public class MyLinkedListTest {
 		myLinkedList.add(mySecondNode);
 		myLinkedList.add(myThirdNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
-				&& myLinkedList.tail.equals(myFirstNode);
+		boolean result = myLinkedList.head.equals(myThirdNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
+						 myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
 
@@ -30,29 +31,9 @@ public class MyLinkedListTest {
 		myLinkedList.append(mySecondNode);
 		myLinkedList.append(myThirdNode);
 		myLinkedList.printMyNodes();
-<<<<<<< HEAD
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
-				&& myLinkedList.tail.equals(myThirdNode);
-		Assert.assertTrue(result);
-	}
-
-	@Test
-	public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
-		MyNode<Integer> myFirstNode = new MyNode<>(56);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		MyLinkedList myLinkedList = new MyLinkedList();
-		myLinkedList.add(myFirstNode);
-		myLinkedList.append(myThirdNode);
-		myLinkedList.insert(myFirstNode, mySecondNode);
-		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
-				&& myLinkedList.tail.equals(myThirdNode);
-=======
-		boolean result = myLinkedList.head.equals(myFirstNode) && 
-				         myLinkedList.head.getNext().equals(mySecondNode) &&
+		boolean result = myLinkedList.head.equals(myFirstNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
 						 myLinkedList.tail.equals(myThirdNode);
->>>>>>> UC3_AppendLinkedListValue
 		Assert.assertTrue(result);
 	}
 }
